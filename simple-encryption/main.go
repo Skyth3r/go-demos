0cdb4f89c2cbb1e9472fd41e219ba7bed7aa9d67
Akash Goswami <Skyth3r@users.noreply.github.com>
2023-02-13 22:29:36 +0000

    added sha256 hashing to simple-encryption
    Before the passphrase variable had to be exactly 32 bytes. Now the passphrase can be of any length, as it is hashed into a 32 byte sha256 hash which is used to create the new cipher.Block

--- simple-encryption/main.go
+++ simple-encryption/main.go
@@ -2,25 +2,24 @@ package main
 
 import (
 	"crypto/aes"
+	"crypto/sha256"
 	"fmt"
 	"log"
 )
 
 func main() {
-
 	plainText := "This is a message to be encrypted"
-	passphrase := "This is a passphrase!!!!"
+	passphrase := "Test passphrase!"
 
 	encryptedMessage := encryptMessage(plainText, passphrase)
-	//decryptedMessage := decryptMessage()
 
 	fmt.Printf("Encrypted message: %s\n", encryptedMessage)
-	//fmt.Printf("decrypted message: %s\n", decryptedMessage)
-
 }
 
 func encryptMessage(message string, pass string) string {
-	cipherBlock, err := aes.NewCipher([]byte(pass))
+	hashedPass := sha256.Sum256([]byte(pass))
+	hashedPassByte := hashedPass[:]
+	cipherBlock, err := aes.NewCipher([]byte(hashedPassByte))
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -28,7 +27,3 @@ func encryptMessage(message string, pass string) string {
 	cipherBlock.Encrypt(cipherText, []byte(message))
 	return string(cipherText)
 }
-
-// func decryptMessage(message string, pass string) string {
-
-// }
