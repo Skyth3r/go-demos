a74ec9de0d8cac1686c34ab20e7bb7c10c64e2c4
Akash Goswami <Skyth3r@users.noreply.github.com>
2023-02-13 23:21:03 +0000

    added decrypt function to simple-encryption (wip)
    added incomplete decryptMessage func to simple-encryption

--- simple-encryption/main.go
+++ simple-encryption/main.go
@@ -8,12 +8,14 @@ import (
 )
 
 func main() {
-	plainText := "This is a message to be encrypted"
+	plainText := "This is a message to be encrypted & decrypted"
 	passphrase := "Test passphrase!"
 
 	encryptedMessage := encryptMessage(plainText, passphrase)
+	decryptedMessage := decryptMessage(encryptedMessage, passphrase)
 
 	fmt.Printf("Encrypted message: %s\n", encryptedMessage)
+	fmt.Printf("Decrypted message: %s\n", decryptedMessage)
 }
 
 func encryptMessage(message string, pass string) string {
@@ -25,5 +27,16 @@ func encryptMessage(message string, pass string) string {
 	}
 	cipherText := make([]byte, len(message))
 	cipherBlock.Encrypt(cipherText, []byte(message))
+	return string(cipherText)
+}
+func decryptMessage(message string, pass string) string {
+	hashedPass := sha256.Sum256([]byte(pass))
+	hashedPassByte := hashedPass[:]
+	cipherBlock, err := aes.NewCipher([]byte(hashedPassByte))
+	if err != nil {
+		log.Fatal(err)
+	}
+	cipherText := make([]byte, len(message))
+	cipherBlock.Decrypt(cipherText, []byte(message))
 	return string(cipherText)
 }
