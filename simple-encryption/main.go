fbe9eefc0a5e38fd635c563133706e8ffc7a3f39
Akash Goswami <Skyth3r@users.noreply.github.com>
2023-02-19 23:01:19 +0000

    updated simple-encyption
    updated to use cipher.NewGCM instead. Still having issues with decryption

--- simple-encryption/main.go
+++ simple-encryption/main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
 	"crypto/sha256"
 	"fmt"
+	"io"
 	"log"
 )
 
@@ -14,29 +17,57 @@ func main() {
 	encryptedMessage := encryptMessage(plainText, passphrase)
 	decryptedMessage := decryptMessage(encryptedMessage, passphrase)
 
-	fmt.Printf("Encrypted message: %s\n", encryptedMessage)
-	fmt.Printf("Decrypted message: %s\n", decryptedMessage)
+	fmt.Printf("Plain Text: %s\n", plainText)
+	fmt.Printf("Encrypted message: %x\n", encryptedMessage)
+	fmt.Printf("Decrypted message: %x\n", decryptedMessage)
+}
+
+func hashPass(pass string) string {
+	sha256Pass := sha256.Sum256([]byte(pass))
+	hashedPassByte := sha256Pass[:]
+	return string(hashedPassByte)
 }
 
 func encryptMessage(message string, pass string) string {
-	hashedPass := sha256.Sum256([]byte(pass))
-	hashedPassByte := hashedPass[:]
-	cipherBlock, err := aes.NewCipher([]byte(hashedPassByte))
+	encrpytionKey := hashPass(pass)
+	cipherBlock, err := aes.NewCipher([]byte(encrpytionKey))
 	if err != nil {
 		log.Fatal(err)
 	}
-	cipherText := make([]byte, len(message))
-	cipherBlock.Encrypt(cipherText, []byte(message))
+
+	aesgcm, err := cipher.NewGCM(cipherBlock)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	non := make([]byte, aesgcm.NonceSize())
+	if _, err := io.ReadFull(rand.Reader, non); err != nil {
+		log.Fatal(err)
+	}
+
+	cipherText := aesgcm.Seal(nil, non, []byte(message), nil)
 	return string(cipherText)
 }
+
 func decryptMessage(message string, pass string) string {
-	hashedPass := sha256.Sum256([]byte(pass))
-	hashedPassByte := hashedPass[:]
-	cipherBlock, err := aes.NewCipher([]byte(hashedPassByte))
+	byteMessage := []byte(message)
+	encrpytionKey := hashPass(pass)
+	cipherBlock, err := aes.NewCipher([]byte(encrpytionKey))
 	if err != nil {
 		log.Fatal(err)
 	}
-	cipherText := make([]byte, len(message))
-	cipherBlock.Decrypt(cipherText, []byte(message))
-	return string(cipherText)
+
+	aesgcm, err := cipher.NewGCM(cipherBlock)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	nonSize := aesgcm.NonceSize()
+	non, cipherText := byteMessage[:nonSize], byteMessage[nonSize:]
+
+	plainText, err := aesgcm.Open(nil, non, cipherText, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return string(plainText)
 }
