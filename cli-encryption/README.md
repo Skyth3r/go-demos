6e8d2717ad0403aea3cc80d6daaf6d758f63a2fc
Akash Goswami <Skyth3r@users.noreply.github.com>
2023-02-24 23:03:27 +0000

    fixed cli-encryption
    added remaining decryption logic to cli-encryption script

--- cli-encryption/README.md
+++ cli-encryption/README.md
@@ -4,4 +4,4 @@ A cli script that uses the crypto library to encrypt and decrypt a message using
 
 ## Usage
 
-The program can be run by entering `go run main.go -e "[MESSAGE_HERE]" "[KEY_HERE]"` to encrypt
+The program can be run by entering `go run main.go -e "[MESSAGE_HERE]" "[KEY_HERE]"` to encrypt messages and `go run main.go -d "[MESSAGE_HERE]" "[KEY_HERE]"` to decrypt messages
--- cli-encryption/main.go
+++ cli-encryption/main.go
@@ -5,6 +5,7 @@ import (
 	"crypto/cipher"
 	"crypto/rand"
 	"crypto/sha256"
+	"encoding/hex"
 	"flag"
 	"fmt"
 	"io"
@@ -16,21 +17,16 @@ func main() {
 
 	inputText := os.Args[2]
 	passphrase := os.Args[3]
-	//inputMessageFlag := flag.String("m", "Default Message", "The message to be encrypted or decrypted. Default: 'Default Message'")
-	//inputKeyFlag := flag.String("k", "Default Key", "The key used to encrypt or decrypt a message. Default: 'Default Key'")
 	encryptFlag := flag.Bool("e", false, "Encrypts the message using the key.")
 	decryptFlag := flag.Bool("d", false, "Decrypts the message using the key.")
 	flag.Parse()
 
 	if *encryptFlag == true {
-		fmt.Printf("Encryption Flag == true\n")
 		encryptedMessage := encryptMessage(inputText, passphrase)
 		fmt.Printf("Encrypted message: %x\n", encryptedMessage)
 	} else if *decryptFlag == true {
-		fmt.Printf("Decryption Flag == true\n")
-		//encryptedMessage := encryptMessage(inputText, passphrase)
-		//decryptedMessage := decryptMessage(inputText, passphrase)
-		//fmt.Printf("Decrypted message: %s\n", decryptedMessage)
+		decryptedMessage := decryptMessage(inputText, passphrase)
+		fmt.Printf("Decrypted message: %s\n", decryptedMessage)
 	} else {
 		fmt.Println("Please try again")
 	}
@@ -63,8 +59,11 @@ func encryptMessage(message string, pass string) string {
 }
 
 func decryptMessage(message string, pass string) string {
-	byteMessage := []byte(message)
 	encrpytionKey := hashPass(pass)
+	encodedMessage, err := hex.DecodeString(message)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	cipherBlock, err := aes.NewCipher([]byte(encrpytionKey))
 	if err != nil {
@@ -77,7 +76,7 @@ func decryptMessage(message string, pass string) string {
 	}
 
 	nonSize := aesgcm.NonceSize()
-	non, cipherText := byteMessage[:nonSize], byteMessage[nonSize:]
+	non, cipherText := encodedMessage[:nonSize], encodedMessage[nonSize:]
 
 	plainText, err := aesgcm.Open(nil, non, cipherText, nil)
 	if err != nil {
