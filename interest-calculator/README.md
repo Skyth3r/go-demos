0cc3b79ed43a88ac0eec6b84851ade4d6bac7a2e
Akash Goswami <Skyth3r@users.noreply.github.com>
2023-02-28 21:46:00 +0000

    updated interest-calculator script to use of flags

--- interest-calculator/README.md
+++ interest-calculator/README.md
@@ -4,6 +4,18 @@
 
 The program can be run by entering `go run main.go [BALANCE_NUMBER] [INTEREST_RATE]"`
 
-**Example usage**
+## Usage
 
-`go run main.go 1000 4.00`
+The program can be run by entering `go run main.go` but using the flags listed below modifies the script's output.
+
+### Flags
+
+The script can be run with a number of flags:
+
+* b - Set the intial balance
+* r - Set the interest rate
+* y - Set number of years compound interest rate
+
+**Example usage with flags**
+
+`go run main.go -b 1000 -r 3 -y 5`
--- interest-calculator/main.go
+++ interest-calculator/main.go
@@ -1,23 +1,22 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"log"
-	"os"
-	"strconv"
+	"math"
 )
 
 func main() {
-	// TODO add flags
-	balance, err := strconv.ParseFloat(os.Args[1], 64)
-	if err != nil {
-		log.Fatal(err)
-	}
-	interestRate, err := strconv.ParseFloat(os.Args[2], 64)
-	if err != nil {
-		log.Fatal(err)
-	}
-	updatedBalance := balance + (balance * (interestRate / 100))
+	years := flag.Float64("y", 1.00, "Number of years interest rate is applied to. Default: 1")
+	balance := flag.Float64("b", 100, "Balance. Default: 100")
+	inputInterestRate := flag.Float64("r", 1.00, "Interest rate. Default: 1%")
+	flag.Parse()
 
-	fmt.Printf("Balance: %.2f\n", updatedBalance)
+	formattedInterestRate := (*inputInterestRate / 100) + 1
+	compoundedInterestRate := math.Pow(formattedInterestRate, *years)
+	projectedBalance := *balance * compoundedInterestRate
+
+	fmt.Printf("Initial balance: £%.2f\n", *balance)
+	fmt.Printf("Interest rate: %.2f%% \n", *inputInterestRate)
+	fmt.Printf("Projected balance: £%.2f\n", projectedBalance)
 }
