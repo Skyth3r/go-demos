d7c9f9113a14f63293194e091f78493883a07fc7
Akash Goswami <Skyth3r@users.noreply.github.com>
2023-03-03 19:13:40 +0000

    added rand-nums-generator

---
+++ rand-nums-generator/README.md
@@ -0,0 +1,7 @@
+# Rand Nums Generator
+
+A cli script that generates a slice of x numbers. 
+
+## Usage
+
+The program can be run by entering `go run main.go x y` where x is the number of numbers to generate and y is the upper limit of the range of numbers generated.
---
+++ rand-nums-generator/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"os"
+	"sort"
+	"strconv"
+)
+
+func main() {
+	sliceSize, _ := strconv.Atoi(os.Args[1])
+	randLimit, _ := strconv.Atoi(os.Args[2])
+	intSlice := make([]int, sliceSize)
+
+	generateNumbers(intSlice, randLimit)
+
+	fmt.Printf("%v\n", intSlice)
+}
+
+func generateNumbers(slice []int, limit int) []int {
+	for index := range slice {
+		slice[index] = rand.Intn(limit) + 1
+	}
+	sort.Ints(slice)
+	return slice
+}
